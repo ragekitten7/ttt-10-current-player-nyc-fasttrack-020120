0b2fa7ececcd722aa8382d455ac7a2bfb32f012f
Sam Bryant <bryantsj@gmail.com>
2020-02-13 02:48:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,10 @@
 def turn_count(board)
   counter = 0
   board.each do |token|
-  token == "X" || token == "O"
+    token == "X" || token == "O"
       counter += 1
     end
   end
-  counter
-end
 
 def current_player(board, index, token)
 end
